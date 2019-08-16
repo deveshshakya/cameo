a05a82132e0ac1204b35521e30bef90f4947d21f
Devesh Shakya <deveshshakya773@gmail.com>
2019-08-16 15:52:41 +0530

    updated

--- cameo/cameo.py
+++ cameo/cameo.py
@@ -1,8 +1,5 @@
 import cv2
-# import filters
-from managers import CaptureManager, WindowManager
-# from trackers import FaceTracker
-# import rects
+from cameo.managers import CaptureManager, WindowManager
 
 
 class Cameo(object):
@@ -11,26 +8,12 @@ class Cameo(object):
 
         self._windowManager = WindowManager('Cameo', self.onKeypress)
         self._captureManager = CaptureManager(cv2.VideoCapture(0), self._windowManager, True)
-        # self._curveFilter = filters.BGRProviaCurveFilter()
-        # self._faceTracker = FaceTracker()
-        # self._shouldDrawDebugRect = False
 
     def run(self):
         """Run the main loop."""
         self._windowManager.createWindow()
         while self._windowManager.isWindowCreated:
             self._captureManager.enterFrame()
-            frame = self._captureManager.frame
-
-            # self._faceTracker.update(frame)
-            # faces = self._faceTracker.faces
-
-            # filters.strokeEdges(frame, frame)
-            # self._curveFilter.apply(frame, frame)
-
-            # if self._shouldDrawDebugRect:
-            #     self._faceTracker.drawDebugRects(frame)
-
             self._captureManager.exitFrame()
             self._windowManager.processEvents()
 
@@ -46,16 +29,12 @@ class Cameo(object):
         # Space
         if keyCode == 32:
             self._captureManager.writeImage('screenshot.jpg')
-        # # x
-        # if keyCode == 120:
-        #     self._shouldDrawDebugRect = not self._shouldDrawDebugRect
         # Tab
         elif keyCode == 9:
             if not self._captureManager.isWritingVideo:
                 self._captureManager.startWritingVideo('screencast.avi')
             else:
                 self._captureManager.stopWritingVideo()
-
         # ESC
         elif keyCode == 27:
             self._windowManager.destroyWindow()
